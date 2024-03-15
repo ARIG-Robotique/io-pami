365b996026129b98c286c67210a1b27eb09db415
Gregory DEPUILLE <gregory.depuille@gmail.com>
2024-03-15 08:30:11 +0100

    Change micro switch read polarity

--- src/main.cpp
+++ src/main.cpp
@@ -14,9 +14,9 @@
 #define GP2D2 A2
 #define GP2D3 A7
 
-bool val_input1 = 0;
-bool val_input2 = 0;
-bool val_input3 = 0;
+bool val_input1 = false;
+bool val_input2 = false;
+bool val_input3 = false;
 uint8_t val_gp2d1 = 0;
 uint8_t val_gp2d2 = 0;
 uint8_t val_gp2d3 = 0;
@@ -29,38 +29,38 @@ Servo servo2;
 
 void processResponse(bool wire) {
     if (wire) {
-    I2C_writeAnything(val_input1);
-    I2C_writeAnything(val_input2);
-    I2C_writeAnything(val_input3);
-    I2C_writeAnything(val_gp2d1);
-    I2C_writeAnything(val_gp2d2);
-    I2C_writeAnything(val_gp2d3);
+        I2C_writeAnything(val_input1);
+        I2C_writeAnything(val_input2);
+        I2C_writeAnything(val_input3);
+        I2C_writeAnything(val_gp2d1);
+        I2C_writeAnything(val_gp2d2);
+        I2C_writeAnything(val_gp2d3);
     } else {
 #ifdef DEBUG
-    Serial.println(val_input1);
-    Serial.println(val_input2);
-    Serial.println(val_input3);
-    Serial.println(val_gp2d1);
-    Serial.println(val_gp2d2);
-    Serial.println(val_gp2d3);
+        Serial.println(val_input1);
+        Serial.println(val_input2);
+        Serial.println(val_input3);
+        Serial.println(val_gp2d1);
+        Serial.println(val_gp2d2);
+        Serial.println(val_gp2d3);
 #endif
-  }
+    }
 }
 
 void processRequest(int length, boolean wire) {
-  char c = wire ? Wire.read() : Serial.read();
+    char c = wire ? Wire.read() : Serial.read();
 
     switch (c) {
-  case 'S':
+        case 'S':
             if (wire) {
-      I2C_readAnything(val_servo1);
-      I2C_readAnything(val_servo2);
+                I2C_readAnything(val_servo1);
+                I2C_readAnything(val_servo2);
             } else {
 #ifdef DEBUG
-      val_servo1 = Serial.parseInt(SKIP_ALL, '\n');
-      val_servo2 = Serial.parseInt(SKIP_ALL, '\n');
+                val_servo1 = Serial.parseInt(SKIP_ALL, '\n');
+                val_servo2 = Serial.parseInt(SKIP_ALL, '\n');
 #endif
-    }
+            }
 
             servo1.writeMicroseconds(val_servo1);
             servo2.writeMicroseconds(val_servo2);
@@ -69,13 +69,13 @@ void processRequest(int length, boolean wire) {
                 servo1.attach(SERVO1, 500, 2500);
                 servo2.attach(SERVO2, 500, 2500);
                 servo_init = true;
-    }
-    break;
+            }
+            break;
 
-  case 'D':
-    servo1.detach();
-    servo2.detach();
-    servo_init = false;
+        case 'D':
+            servo1.detach();
+            servo2.detach();
+            servo_init = false;
             break;
 
         case 'F': // Serial only
@@ -84,12 +84,12 @@ void processRequest(int length, boolean wire) {
                 break;
             }
 
-  default:
+        default:
 #ifdef DEBUG
-    Serial.print(F("Requete inconnue "));
-    Serial.println(c);
+            Serial.print(F("Requete inconnue "));
+            Serial.println(c);
 #endif
-    break;
+            break;
     }
 }
 
@@ -112,27 +112,29 @@ void setup() {
     Serial.begin(115200);
 #endif
 
-  pinMode(INPUT1, INPUT_PULLUP);
-  pinMode(INPUT2, INPUT_PULLUP);
-  pinMode(INPUT3, INPUT_PULLUP);
+    pinMode(INPUT1, INPUT_PULLUP);
+    pinMode(INPUT2, INPUT_PULLUP);
+    pinMode(INPUT3, INPUT_PULLUP);
 
-  Wire.begin(I2C_ADD);
-  Wire.onReceive(I2C_RxHandler);
-  Wire.onRequest(I2C_TxHandler);
+    Wire.begin(I2C_ADD);
+    Wire.onReceive(I2C_RxHandler);
+    Wire.onRequest(I2C_TxHandler);
 }
 
 void loop() {
 #if defined(DEBUG)
     if (Serial.available()) {
-    processRequest(7, false);
-  }
+        processRequest(7, false);
+    }
 #endif
 
-  val_input1 = digitalRead(INPUT1);
-  val_input2 = digitalRead(INPUT2);
-  val_input3 = digitalRead(INPUT3);
+    // The micro switch is connected on ground.
+    // The inputs on controller have a pullup configured by hardware
+    val_input1 = digitalRead(INPUT1) == LOW;
+    val_input2 = digitalRead(INPUT2) == LOW;
+    val_input3 = digitalRead(INPUT3) == LOW;
 
-  val_gp2d1 = readGP2D(GP2D1);
-  val_gp2d2 = readGP2D(GP2D2);
-  val_gp2d3 = readGP2D(GP2D3);
+    val_gp2d1 = readGP2D(GP2D1);
+    val_gp2d2 = readGP2D(GP2D2);
+    val_gp2d3 = readGP2D(GP2D3);
 }
