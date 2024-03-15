43d200d593e7698a4fd2a5228394baa5b5226ed6
Gregory DEPUILLE <gregory.depuille@gmail.com>
2024-03-15 08:29:27 +0100

    CLion standard format

--- src/main.cpp
+++ src/main.cpp
@@ -27,19 +27,15 @@ bool servo_init = false;
 Servo servo1;
 Servo servo2;
 
-void processResponse(bool wire)
-{
-  if (wire)
-  {
+void processResponse(bool wire) {
+    if (wire) {
     I2C_writeAnything(val_input1);
     I2C_writeAnything(val_input2);
     I2C_writeAnything(val_input3);
     I2C_writeAnything(val_gp2d1);
     I2C_writeAnything(val_gp2d2);
     I2C_writeAnything(val_gp2d3);
-  }
-  else
-  {
+    } else {
 #ifdef DEBUG
     Serial.println(val_input1);
     Serial.println(val_input2);
@@ -51,34 +47,28 @@ void processResponse(bool wire)
   }
 }
 
-void processRequest(int length, boolean wire)
-{
+void processRequest(int length, boolean wire) {
   char c = wire ? Wire.read() : Serial.read();
 
-  switch (c)
-  {
+    switch (c) {
   case 'S':
-    if (wire)
-    {
+            if (wire) {
       I2C_readAnything(val_servo1);
       I2C_readAnything(val_servo2);
-    }
-    else
-    {
+            } else {
 #ifdef DEBUG
       val_servo1 = Serial.parseInt(SKIP_ALL, '\n');
       val_servo2 = Serial.parseInt(SKIP_ALL, '\n');
 #endif
     }
 
-    servo1.writeMicroseconds(val_servo1);
-    servo2.writeMicroseconds(val_servo2);
+            servo1.writeMicroseconds(val_servo1);
+            servo2.writeMicroseconds(val_servo2);
 
-    if (!servo_init)
-    {
-      servo1.attach(SERVO1, 500, 2500);
-      servo2.attach(SERVO2, 500, 2500);
-      servo_init = true;
+            if (!servo_init) {
+                servo1.attach(SERVO1, 500, 2500);
+                servo2.attach(SERVO2, 500, 2500);
+                servo_init = true;
     }
     break;
 
@@ -86,14 +76,13 @@ void processRequest(int length, boolean wire)
     servo1.detach();
     servo2.detach();
     servo_init = false;
-    break;
+            break;
 
-  case 'F': // Serial only
-    if (!wire)
-    {
-      processResponse(false);
-      break;
-    }
+        case 'F': // Serial only
+            if (!wire) {
+                processResponse(false);
+                break;
+            }
 
   default:
 #ifdef DEBUG
@@ -101,30 +90,26 @@ void processRequest(int length, boolean wire)
     Serial.println(c);
 #endif
     break;
-  }
+    }
 }
 
-void I2C_RxHandler(int numBytes)
-{
-  processRequest(numBytes, true);
+void I2C_RxHandler(int numBytes) {
+    processRequest(numBytes, true);
 }
 
-void I2C_TxHandler(void)
-{
-  processResponse(true);
+void I2C_TxHandler(void) {
+    processResponse(true);
 }
 
 // https://swanrobotics.com/projects/gp2d12_project/
-uint8_t readGP2D(uint8_t pin)
-{
-  int val = analogRead(pin);
-  return round(6787.0 / (val - 3.0)) - 4.0;
+uint8_t readGP2D(uint8_t pin) {
+    int val = analogRead(pin);
+    return round(6787.0 / (val - 3.0)) - 4.0;
 }
 
-void setup()
-{
+void setup() {
 #ifdef DEBUG
-  Serial.begin(115200);
+    Serial.begin(115200);
 #endif
 
   pinMode(INPUT1, INPUT_PULLUP);
@@ -136,11 +121,9 @@ void setup()
   Wire.onRequest(I2C_TxHandler);
 }
 
-void loop()
-{
+void loop() {
 #if defined(DEBUG)
-  if (Serial.available())
-  {
+    if (Serial.available()) {
     processRequest(7, false);
   }
 #endif
